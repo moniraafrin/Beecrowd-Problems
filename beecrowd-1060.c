d6a9004dd1521d29ebe7516fccdb369a76f24df9
Monira Akter <akter.monira.cse@gmail.com>
2024-02-02 22:43:46 +0600

    for,if

---
+++ beecrowd-1060.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  float x;
+  int i,sum=0;
+  for(i=1;i<=6;i++)
+  {
+      scanf("%f",&x);
+      if(x>0)
+      {
+          sum++;
+      }
+  }
+      
+
+    printf("%d valores positivos\n",sum);
+
+    return 0;
+}
